3770e74bb4e28b224ec3987e69c1731c9c818f68
michellelliu <137734177+michellelliu@users.noreply.github.com>
2024-05-02 11:18:51 +1200

    Add files via upload
    doing a practice version of a similar app - this is an emoji match app. Currently, there is an error in my code because the images on some of the buttons aren't loading in

---
+++ emoji_match.py
@@ -0,0 +1,61 @@
+"""
+Title: Emoji Match
+Author: Michelle Liu
+Date: 09-04-24
+Version: 1
+
+"""
+# --------------------
+# Imports
+# --------------------
+
+import os
+from random import shuffle
+from guizero import App, Box, Picture, PushButton
+
+
+# --------------------
+# Variables
+# --------------------
+
+# set the path to the emoji folder on your computer
+emojis_dir = "emojis"
+emojis = [os.path.join(emojis_dir, f) for f in os.listdir(emojis_dir)]
+shuffle(emojis)
+
+
+# --------------------
+# Functions
+# --------------------
+def setup_round():
+    for picture in pictures:
+        picture.image = emojis.pop()
+
+    for buttom in buttons:
+        button.image = emojis.pop()
+
+# --------------------
+# App
+# --------------------
+
+#print (emojis)
+
+app= App("emoji match")
+
+pictures_box = Box(app, layout="grid", border=True)
+buttons_box = Box(app, layout="grid", border=True)
+
+pictures = []
+buttons = []
+
+for x in range(0,3):
+    for y in range(0,3):
+        picture = Picture(pictures_box, grid=[x,y])
+        pictures.append(picture)
+
+        button = PushButton(buttons_box, grid=[x,y])
+        buttons.append(button)
+
+setup_round()
+
+app.display()
