28fdc95c3d5491550892024d2cab3fc5f31fb0e4
michellelliu <137734177+michellelliu@users.noreply.github.com>
2024-05-07 01:34:05 +0000

    puesdocode for language app

---
+++ languageapp_v1.py
@@ -0,0 +1,37 @@
+"""
+Title: language app
+Author: Michelle Liu
+Date: 06/05/24
+Version: 1
+"""
+
+# -----------------
+# Imports
+# -----------------
+
+from guizero import App
+
+# -----------------
+# Variables
+# -----------------
+
+#set path to images foler
+
+# -----------------
+# Functions
+# -----------------
+
+# function to set up one round
+
+# function to match words with images
+
+# function to set up timer
+
+# -----------------
+# App
+# -----------------
+
+# positioning app
+
+app = App("language app")
+app.display()
---
+++ main.py
@@ -0,0 +1,1 @@
+import languageapp_v1
